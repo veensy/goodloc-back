d7e68bc46b0c8f1e480306dc5103076a646c2128
keveens belvilus <veensy@keveenss-MacBook-Pro.local>
2019-02-14 23:43:26 +0100

    mail

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const jwt = require("jwt-simple");
 const config = require("../config");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const tokenForUser = user => {
   const timestamp = new Date().getTime();
@@ -9,59 +10,61 @@ const tokenForUser = user => {
 };
 
 exports.forgotPassword = (req, res, next) => {
-  console.log(req.body, "email");
-
   const email = req.body.email;
   const mailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!email || email === "" || !email.match(mailFormat)) {
-    res.json("valid email required");
+  if (!email) {
+    res.json("You must provide an email");
+  } else if (!email.match(mailFormat)) {
+    res.json("The email you provide is not valid");
   }
-  console.log(req.body.email);
 
-  User.findOne(
-    {
-      email: email
-    },
-    (err, user) => {
-      if (err) {
-        console.log("email not in database", user);
-        res.json("email not in db");
-      } else if (user) {
-        const newToken = tokenForUser(user);
-        console.log(newToken);
+  User.findOne({
+    email: email
+  }).then(user => {
 
-        user.update({
-          resetPasswordToken: newToken,
-          resetPasswordExpires: Date.now() + 360000
-        });
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: `${process.env.EMAIL_ADDRESS}`,
-            password: `${process.env.EMAIL_PASSWORD}`
-          }
-        });
-        const mailOption = {
-          from: "veensy@gmail.com",
-          to: `${user.email}`,
-          subject: `Link To Reset Password`,
-          text:
-            `You received this email because you (or someone else ) have requested the reset of the password for your account.${" "}` +
-            `Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:${" "}` +
-            `http://localhost/3090/reset/${newToken} ${" "}` +
-            `Ìf you did not request this, please ignore this email and your password will remain unchanged.`
-        };
-        console.log("sending email");
-        transporter.sendMail(mailOption, (err, response) => {
-          if (err) {
-            console.error("there was an error", err);
-            return next(err);
-          } else {
-            console.log("here is the res: ", response);
-            res.status(200).json("recovery email sent");
-          }
-        });
-      }
+    if (user === null) {
+      console.log("Email not in database", user);
+      res.json("Email not in db");
+    } else if (user) {
+      const newToken = tokenForUser(user);
+      console.log(newToken);
+
+      user.update({
+        resetPasswordToken: newToken,
+        resetPasswordExpires: Date.now() + 360000
+      });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        auth: {
+          user: `${process.env.EMAIL_ADDRESS}`,
+          pass: `${process.env.EMAIL_PASSWORD}`
+        }
+      });
+      console.log(transporter);
+
+      const mailOption = {
+        from: "demo@gmail.com",
+        to: `${user.email}`,
+        subject: `Link To Reset Password`,
+        text:
+          `You received this email because you (or someone else ) have requested the reset of the password for your account.${" "}` +
+          `Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:${" "}` +
+          `http://localhost/3090/reset/${newToken} ${" "}` +
+          `Ìf you did not request this, please ignore this email and your password will remain unchanged.`
+      };
+      console.log("sending email");
+      transporter.sendMail(mailOption, (err, response) => {
+        console.log(mailOption);
+
+        if (err) {
+          console.error("there was an error", err);
+          return next(err);
+        } else {
+          console.log("here is the res: ", response);
+          res.status(200).json("Recovery email sent");
+        }
+      });
     }
-  );
+  });
 };
